0fbc2f36a32dd45df7485513bcefde1b7fdd01ec
hammiche <hammicheibrahimm@gmail.com>
2024-02-19 09:27:16 -0800

    create stackTest

--- src/test/java/StackTest.java
+++ src/test/java/StackTest.java
@@ -1,2 +1,33 @@
+import org.example.Stack;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class StackTest {
+    Stack pile;
+    @BeforeEach
+    void setUp(){
+         pile = new Stack();
+
+    }
+    @Test
+    void PeekTest(){
+        assertThrows(IllegalStateException.class, pile::peek);
+    }
+    @Test
+    void PushTest(){
+        pile.push(5);
+        assertEquals(5,pile.peek());
+        assertEquals(1,pile.size());
+    }
+
+    @Test
+    void PopTest(){
+        pile.pop();
+        assertThrows(IllegalStateException.class, pile::peek);
+    }
+
+
 }
